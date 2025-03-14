68cdf5413ecf023b47b36d670d91e65baf25352d
Ömer Faruk Karaköy <156546037+OmerFarukKarakoy@users.noreply.github.com>
2025-03-14 04:32:35 +0300

    convert_movies.py

--- Chat_rasa/convert_movies.py
+++ Chat_rasa/convert_movies.py
@@ -1,16 +1,11 @@
 import pandas as pd
 import json
 
-# CSV dosyasının tam yolunu belirt
-file_path = r"C:\Users\omerf\FilmVeriSeti\movie.csv"  # Windows için 'r' ekledim
-
-# CSV dosyasını yükle
+file_path = r"C:\Users\omerf\FilmVeriSeti\movie.csv"   
 df = pd.read_csv(file_path)
 
-# JSON formatına dönüştür
 movies_json = df[['title', 'genres']].to_dict(orient='records')
 
-# JSON dosyasını Rasa klasörüne kaydet
 output_path = "movies.json"
 with open(output_path, "w", encoding="utf-8") as f:
     json.dump(movies_json, f, indent=4, ensure_ascii=False)
